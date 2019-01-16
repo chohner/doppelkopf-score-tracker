31821f61e0e5d8d27180fff4103733d751255487
chohner <mail@chohner.com>
2019-01-16 12:46:08 +0100

    better help modal

--- src/components/helpModal.jsx
+++ src/components/helpModal.jsx
@@ -3,13 +3,20 @@ import { Modal} from 'semantic-ui-react';
 
 const HelpModal = (props) => {
   return (
-    <Modal basic trigger={props.trigger} centered={false}>
-      <Modal.Header>Help</Modal.Header>
+    <Modal basic trigger={props.trigger} centered={false} closeIcon>
+      <Modal.Header>Doko Butler</Modal.Header>
       <Modal.Content>
         <Modal.Description>
-          <p>You can add a game by entering the game score and winners. Solos points are calculated automatically.</p>
-          <p>You can edit a previous game by clicking on the corresponding score and hitting the edit icon. It will populate the bottom row, adding the game will edit in place.</p>
-          <p>You can edit player names by clicking on them.</p>
+          <p>Doko Butler is a little web app that helps you keep track of your Doppelkopf score.</p>
+          <ul>
+            <li>To change a player name, simply click on a name and start typing</li>
+            <li>To add a game, use the bottom row to enter the score and select who won that round</li>
+            <li>To add/remove players, click Players and select Add or Remove. Player count is limited between 4 and 7 and may be changed any time.</li>
+            <li>To edit a previous game, select the corresponding score and click the edit icon. It will populate the bottom row with the game data</li>
+            <li>To reset everything, click the red RESET on the top right and confirm</li>
+            <li>Games with a score of 0 must have no winners, games with a score >0 must have a least one winner</li>
+          </ul>
+          <p>Don't worry if you accidentally reload or close the page - Doko Butler will keep the score.</p>
         </Modal.Description>
       </Modal.Content>
     </Modal>
